6a21d4ada673321667dd91075d6634dbc735c3b7
wugelis <wergelis@msn.com>
2021-06-21 19:07:21 +0800

    初次簽入

---
+++ BlazorServerIMemoryCache1/Models/CounterModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerIMemoryCache1.Models
+{
+    public class CounterModel
+    {
+        public int? Counter { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## Blazor 狀態保存文章範例程式碼
+
+# 使用 .NET Core/5 內建的 IMemoryCache 保存 COUNT 狀態
